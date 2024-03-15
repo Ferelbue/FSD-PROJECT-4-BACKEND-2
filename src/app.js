f0c2ce24623b46b106e931f3a65668ceae87513b
Fernando <ferelbue@gmail.com>
2024-03-15 12:01:19 +0100

    feat:cors added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-
+import cors from "cors";
 import express from "express";
 import router from "./router.js";
 
 export const app = express();
 
+app.use(cors());
 
 app.use(express.json());
-
 
 app.get('/api/healthy', (req, res) => {
     res.status(200).json(
